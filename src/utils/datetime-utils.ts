3c78030d64ac7596b71a595d3be285f87a39541a
Ke Jin <ke.jin@laserfiche.com>
2022-02-17 19:27:00 -0800

    revert

--- src/utils/datetime-utils.ts
+++ src/utils/datetime-utils.ts
@@ -34,7 +34,17 @@ export class DatetimeUtils {
       return (norm < 10 ? '0' : '') + norm;
     };
     return (
-    'test should return'
+      date.getFullYear() +
+      '-' +
+      pad(date.getMonth() + 1) +
+      '-' +
+      pad(date.getDate()) +
+      'T' +
+      pad(date.getHours()) +
+      ':' +
+      pad(date.getMinutes()) +
+      ':' +
+      pad(date.getSeconds())
     );
   }
 
