4845dd9ff9dbe6f6f4aaf9cfa3d66a57e9873dd5
Ke Jin <ke.jin@laserfiche.com>
2022-02-17 21:44:57 -0800

    revert

--- src/utils/datetime-utils.spec.ts
+++ src/utils/datetime-utils.spec.ts
@@ -1,4 +1,4 @@
-import { DatetimeUtils } from './datetime-utils.js';
+import { DatetimeUtils } from './datetime-utils';
 
 describe('DatetimeUtils', () => {
 
@@ -12,14 +12,12 @@ describe('DatetimeUtils', () => {
     const dateNoOffset: Date = new Date(deserializedDate);
 
     const serializedDate: string | undefined = DatetimeUtils.serializeDateValue(dateNoOffset);
-    const isValid = DatetimeUtils.isValidDate(dateNoOffset)
-    expect(isValid).toBeTruthy();
+    expect(serializedDate).toEqual('2021-03-25T00:00:00');
   });
 
   it('should serialize a datetime with no offset and not change the value', () => {
     const originalDatetimeString: string = '2021-03-25T01:00:00';
     const datetimeNoOffset: Date = new Date(originalDatetimeString);
-    console.log("time zone", Intl.DateTimeFormat().resolvedOptions().timeZone)
 
     const serializedDatetime: string | undefined = DatetimeUtils.serializeDateValue(datetimeNoOffset);
     expect(serializedDatetime).toEqual('2021-03-25T01:00:00');
