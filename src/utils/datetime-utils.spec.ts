ebf7b62a35c6c178006d43448b7e7ef27e0b6128
Ke Jin <ke.jin@laserfiche.com>
2022-02-17 19:14:33 -0800

    only test 1

--- src/utils/datetime-utils.spec.ts
+++ src/utils/datetime-utils.spec.ts
@@ -41,12 +41,7 @@ describe('DatetimeUtils', () => {
         pad(date.getMonth() + 1) +
         '-' +
         pad(date.getDate()) +
-        'T' +
-        pad(date.getHours()) +
-        ':' +
-        pad(date.getMinutes()) +
-        ':' +
-        pad(date.getSeconds())
+        'T' 
       );
     }
     const serializedDate: string | undefined = test(dateNoOffset);
@@ -56,137 +51,21 @@ describe('DatetimeUtils', () => {
   it('should serialize a datetime with no offset and not change the value', () => {
     const originalDatetimeString: string = '2021-03-25T01:00:00';
     const datetimeNoOffset: Date = new Date(originalDatetimeString);
-    function test(date) {
-      if (!date || !DatetimeUtils.isValidDate(date)) {
-        return undefined;
-      }
-    
-      const offset: number = date.getTimezoneOffset();
-      if (offset === 0) {
-        return date.toISOString();
-      }
-    
-      const pad: (num: number) => string = (num: number) => {
-        const norm = Math.floor(Math.abs(num));
-        return (norm < 10 ? '0' : '') + norm;
-      };
-      console.log(date.getFullYear() +
-      '-' +
-      pad(date.getMonth() + 1) +
-      '-' +
-      pad(date.getDate()) +
-      'T' +
-      pad(date.getHours()) +
-      ':' +
-      pad(date.getMinutes()) +
-      ':' +
-      pad(date.getSeconds()))
-      return (
-        date.getFullYear() +
-        '-' +
-        pad(date.getMonth() + 1) +
-        '-' +
-        pad(date.getDate()) +
-        'T' +
-        pad(date.getHours()) +
-        ':' +
-        pad(date.getMinutes()) +
-        ':' +
-        pad(date.getSeconds())
-      );
-    }
-    const serializedDatetime: string | undefined = test(datetimeNoOffset);
+    const serializedDatetime: string | undefined = DatetimeUtils.serializeDateValue(datetimeNoOffset);
     expect(serializedDatetime).toEqual('2021-03-25T01:00:00');
   });
 
   it('should serialize a date with an offset and remove the offset', () => {
     const originalDateString: string = '2021-03-25T00:00:00-07:00';
     const dateWithOffset: Date = new Date(originalDateString);
-    function test(date) {
-      if (!date || !DatetimeUtils.isValidDate(date)) {
-        return undefined;
-      }
-    
-      const offset: number = date.getTimezoneOffset();
-      if (offset === 0) {
-        return date.toISOString();
-      }
-    
-      const pad: (num: number) => string = (num: number) => {
-        const norm = Math.floor(Math.abs(num));
-        return (norm < 10 ? '0' : '') + norm;
-      };
-      console.log(date.getFullYear() +
-      '-' +
-      pad(date.getMonth() + 1) +
-      '-' +
-      pad(date.getDate()) +
-      'T' +
-      pad(date.getHours()) +
-      ':' +
-      pad(date.getMinutes()) +
-      ':' +
-      pad(date.getSeconds()))
-      return (
-        date.getFullYear() +
-        '-' +
-        pad(date.getMonth() + 1) +
-        '-' +
-        pad(date.getDate()) +
-        'T' +
-        pad(date.getHours()) +
-        ':' +
-        pad(date.getMinutes()) 
-      );
-    }
-    const serializedDate: string | undefined = test(dateWithOffset);
-    console.log("heree", test, test(dateWithOffset))
+    const serializedDate: string | undefined = DatetimeUtils.serializeDateValue(dateWithOffset);
     expect(serializedDate).toEqual('2021-03-25T00:00:00');
   });
 
   it('should serialize a datetime with an offset and remove the offset', () => {
     const originalDatetimeString: string = '2021-03-25T01:00:00-07:00';
     const datetimeWithOffset: Date = new Date(originalDatetimeString);
-    function test(date) {
-      if (!date || !DatetimeUtils.isValidDate(date)) {
-        return undefined;
-      }
-    
-      const offset: number = date.getTimezoneOffset();
-      if (offset === 0) {
-        return date.toISOString();
-      }
-    
-      const pad: (num: number) => string = (num: number) => {
-        const norm = Math.floor(Math.abs(num));
-        return (norm < 10 ? '0' : '') + norm;
-      };
-      console.log(date.getFullYear() +
-      '-' +
-      pad(date.getMonth() + 1) +
-      '-' +
-      pad(date.getDate()) +
-      'T' +
-      pad(date.getHours()) +
-      ':' +
-      pad(date.getMinutes()) +
-      ':' +
-      pad(date.getSeconds()))
-      return (
-        date.getFullYear() +
-        '-' +
-        pad(date.getMonth() + 1) +
-        '-' +
-        pad(date.getDate()) +
-        'T' +
-        pad(date.getHours()) +
-        ':' +
-        pad(date.getMinutes()) +
-        ':' +
-        pad(date.getSeconds())
-      );
-    }
-    const serializedDatetime: string | undefined = test(datetimeWithOffset);
+    const serializedDatetime: string | undefined = DatetimeUtils.serializeDateValue(datetimeWithOffset);
     expect(serializedDatetime).toEqual('2021-03-25T01:00:00');
   });
 
