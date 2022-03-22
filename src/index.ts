865b718962cc8181a874085d264916d568f4ad8e
Ke Jin <ke.jin@laserfiche.com>
2022-03-22 11:19:59 -0700

    export namesspace

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export * from './utils/url-utils.js';
+export * as UrlUtils from './utils/url-utils.js';
 export * as LfMetadataValidationUtils from './utils/validation-utils/validation-utils-public-api.js';
 export * as LfMetadataDatetimeUtils from './utils/datetime-utils.js';
 export * from './utils/lf-localization.service.js';
-export * from './utils/mime-type-utils.js';
-export * from './utils/path-utils.js';
-export * from './utils/time-utils.js';
+export * as MimeTypeUtils from './utils/mime-type-utils.js';
+export * as PathUtils from './utils/path-utils.js';
+export * as TimeUtils from './utils/time-utils.js';
 export * from './utils/validate-defined.js';
