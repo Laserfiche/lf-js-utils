9f2af41a2dff37259b030cd1869bb532ad1d4cec
alexgomezlf <99926015+alexgomezlf@users.noreply.github.com>
2022-04-12 16:18:36 -0700

    update params (#16)
    * update params
    
    * update major version
    
    * try update branch names for push/PR
    
    * update branch policy in yaml
    
    * only do branch \d+.x
    
    * change " to '
    
    Co-authored-by: alexandria.gomez <alexandria.gomez@WATSONVILLE>

--- src/utils/lf-localization.service.ts
+++ src/utils/lf-localization.service.ts
@@ -254,16 +254,17 @@ export class LfLocalizationService implements ILocalizationService {
   }
 
   private formatString(stringToFormat: string, params?: string[]): string {
-    const expectedParams: RegExpMatchArray = stringToFormat.match(/\$\{\d*\}/g) ?? [];
+    const expectedParams: RegExpMatchArray = stringToFormat.match(/\{\d*\}/g) ?? [];
     const expectedNumParams: number = new Set(expectedParams).size;
     if ((expectedNumParams > 0 && params?.length !== expectedNumParams)
       || (expectedNumParams === 0 && params && params.length > 0)) {
       throw new Error(`Expected ${expectedNumParams} arguments. Actual arguments: ${params?.length ?? '0'}.`);
     }
+
     if (params && params.length > 0) {
       for (let i = 0; i < params.length; i++) {
         const replacement: string = params[i];
-        const varRegex: RegExp = new RegExp(`\\$\\{${i}\\}`, 'g');
+        const varRegex: RegExp = new RegExp(`\\{${i}\\}`, 'g');
         stringToFormat = stringToFormat.replace(varRegex, replacement);
       }
     }
