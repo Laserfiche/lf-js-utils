f811e1abc9b455bf8f586ac5275b7426d445edc0
Ke Jin <ke.jin@laserfiche.com>
2022-04-18 09:50:09 -0700

    format document

--- src/utils/access-token-utils.ts
+++ src/utils/access-token-utils.ts
@@ -2,134 +2,134 @@
  * Laserfiche Cloud endpoints
  */
 export interface LfEndpoints {
-    webClientUrl: string;
-    wsignoutUrl: string;
-    repositoryApiBaseUrl: string;
+  webClientUrl: string;
+  wsignoutUrl: string;
+  repositoryApiBaseUrl: string;
 }
 
 /**
  * Decoded JWT
  */
 export interface JWT {
-    header: object;
-    payload: object;
-    signature: string;
+  header: object;
+  payload: object;
+  signature: string;
 }
 
 /**
  * Returns Laserfiche account id (customer id) from Laserfiche jwt claims
- * @param lfJwt 
+ * @param lfJwt
  * @returns
  * @example
  * ```typescript
  * const jwt : AccessTokenUtils.JWT = {
- *  header: { "typ": "JWT"},
- *  payload: {"csid" : "123456789"},
- *  signature: "_signature"
+ *  header: { 'typ': 'JWT'},
+ *  payload: {'csid' : '123456789'},
+ *  signature: '_signature'
  * }
  * getAccountIdFromLfJWT(jwt); // '123456789';
  * ```
  */
 export function getAccountIdFromLfJWT(lfJwt: JWT): string {
-    return lfJwt.payload['csid'];
+  return lfJwt.payload['csid'];
 }
 
 /**
  * Returns Laserfiche trustee id (user id) from Laserfiche jwt claims
- * @param lfJwt 
- * @returns 
+ * @param lfJwt
+ * @returns
  * @example
  * ```typescript
  * const jwt : AccessTokenUtils.JWT = {
- *  header: { "typ": "JWT"},
- *  payload: {"trid" : "1008"},
- *  signature: "_signature"
+ *  header: { 'typ': 'JWT'},
+ *  payload: {'trid' : '1008'},
+ *  signature: '_signature'
  * }
  * getTrusteeIdFromLfJWT(jwt); // '1008';
  * ```
  */
 export function getTrusteeIdFromLfJWT(lfJwt: JWT): string {
-    return lfJwt.payload['trid'];
+  return lfJwt.payload['trid'];
 }
 
 /**
  * Returns region-specific Laserfiche Cloud endpoints
- * @param accountId 
+ * @param accountId
  * @param devEnvironmentSubDomain optional dev environment subDomain, such as 'a.clouddev', or 'cloudtest'
- * @returns 
+ * @returns
  * @example
  * ```typescript
- * getLfEndpoints('123456789', 'a.clouddev'); 
-*  // => {
-*  //      webClientUrl: 'https://app.a.clouddev.laserfiche.com/laserfiche',
-*  //      wsignoutUrl: 'https://accounts.a.clouddev.laserfiche.com/WebSTS/?wa=wsignout1.0',
-*  //      repositoryApiBaseUrl: 'https://api.a.clouddev.laserfiche.com/repository/'
-*  //     }
+ * getLfEndpoints('123456789', 'a.clouddev');
+ *  // => {
+ *  //      webClientUrl: 'https://app.a.clouddev.laserfiche.com/laserfiche',
+ *  //      wsignoutUrl: 'https://accounts.a.clouddev.laserfiche.com/WebSTS/?wa=wsignout1.0',
+ *  //      repositoryApiBaseUrl: 'https://api.a.clouddev.laserfiche.com/repository/'
+ *  //     }
  * ```
  */
 export function getLfEndpoints(accountId: string, devEnvironmentSubDomain?: string): LfEndpoints {
-    const regionSpecificHostName = getLfRegionalDomainFromAccountId(accountId, devEnvironmentSubDomain);
-    return {
-        webClientUrl: `https://app.${regionSpecificHostName}/laserfiche`,
-        repositoryApiBaseUrl: `https://api.${regionSpecificHostName}/repository/`,
-        wsignoutUrl: `https://accounts.${regionSpecificHostName}/WebSTS/?wa=wsignout1.0`,
-    };
+  const regionSpecificHostName = getLfRegionalDomainFromAccountId(accountId, devEnvironmentSubDomain);
+  return {
+    webClientUrl: `https://app.${regionSpecificHostName}/laserfiche`,
+    repositoryApiBaseUrl: `https://api.${regionSpecificHostName}/repository/`,
+    wsignoutUrl: `https://accounts.${regionSpecificHostName}/WebSTS/?wa=wsignout1.0`,
+  };
 }
 
 /**
  * Decodes a string of data encoded using Base64 encoding
- * @param base64String 
- * @returns 
+ * @param base64String
+ * @returns
  * @example
  * ```typescript
  * base64toString('dGVzdA=='); // => 'test';
  * ```
  */
 export function base64toString(base64String: string): string {
-    return Buffer.from(base64String, 'base64').toString();
+  return Buffer.from(base64String, 'base64').toString();
 }
 
 /**
  * Parses a base64-encoded jwt
- * @param jwt 
- * @returns 
+ * @param jwt
+ * @returns
  * @example
  * ```typescript
  * const jwtString = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
  * parseAccessToken(jstString);
- *  // => {            
+ *  // => {
  *            header:
  *            {
- *              "alg": "HS256",
- *              "typ": "JWT"
+ *              'alg': 'HS256',
+ *              'typ': 'JWT'
  *             },
  *            payload:
  *            {
- *              "sub": "1234567890",
- *              "name": "John Doe",
- *              "iat": 1516239022
+ *              'sub': '1234567890',
+ *              'name': 'John Doe',
+ *              'iat': 1516239022
  *            },
  *            signature: 'SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
- *          } 
+ *          }
  * ```
  */
 export function parseAccessToken(jwt: string): JWT {
-    const ary = jwt.split('.');
-    const header = JSON.parse(base64toString(ary[0]));
-    const payload = JSON.parse(base64toString(ary[1]));
-    const signature = ary[2];
-    return {
-        header,
-        payload,
-        signature
-    }
+  const ary = jwt.split('.');
+  const header = JSON.parse(base64toString(ary[0]));
+  const payload = JSON.parse(base64toString(ary[1]));
+  const signature = ary[2];
+  return {
+    header,
+    payload,
+    signature,
+  };
 }
 
 /**
  * Returns the Laserfiche regional cloud domains given an account id
- * @param accountId 
+ * @param accountId
  * @param devEnvironmentSubDomain optional dev environment subDomain, such as 'a.clouddev', or 'cloudtest'
- * @returns 
+ * @returns
  * @example
  * ```typescript
  * getLfRegionalDomainFromAccountId('1123456789', 'a.clouddev'); // 'a.clouddev.laserfiche.ca'
@@ -138,24 +138,24 @@ export function parseAccessToken(jwt: string): JWT {
  * ```
  */
 export function getLfRegionalDomainFromAccountId(accountId: string, devEnvironmentSubDomain?: string): string {
-    const environment: string = devEnvironmentSubDomain ?  devEnvironmentSubDomain + '.' : '';
-    // this will make more sense once OAuth is not region specific
-    // right now this is same as hostname passed in
-    if (accountId.length === 9) {
-        return `${environment}laserfiche.com`;
-    } else if (accountId.length === 10 && accountId.slice(0, 1) === '1') {
-        return `${environment}laserfiche.ca`;
-    } else if (accountId.length === 10 && accountId.slice(0, 1) === '2') {
-        return `${environment}eu.laserfiche.com`;
-    } else {
-        throw new Error('Cannot determine account region, length ' + accountId.length);
-    }
+  const environment: string = devEnvironmentSubDomain ? devEnvironmentSubDomain + '.' : '';
+  // this will make more sense once OAuth is not region specific
+  // right now this is same as hostname passed in
+  if (accountId.length === 9) {
+    return `${environment}laserfiche.com`;
+  } else if (accountId.length === 10 && accountId.slice(0, 1) === '1') {
+    return `${environment}laserfiche.ca`;
+  } else if (accountId.length === 10 && accountId.slice(0, 1) === '2') {
+    return `${environment}eu.laserfiche.com`;
+  } else {
+    throw new Error('Cannot determine account region, length ' + accountId.length);
+  }
 }
 
 /**
  * Returns the Laserfiche Cloud dev environment subDomain associated with the provided host name
- * @param urlHostName 
- * @returns 
+ * @param urlHostName
+ * @returns
  * @example
  * ```typescript
  * getLfDevEnvironmentSubDomain('a.clouddev.laserfiche.ca'); // 'a.clouddev'
@@ -164,11 +164,11 @@ export function getLfRegionalDomainFromAccountId(accountId: string, devEnvironme
  * ```
  */
 export function getLfDevEnvironmentSubDomain(urlHostName: string): string {
-    if (urlHostName.includes('clouddev')) {
-        return 'a.clouddev';
-    } else if (urlHostName.includes('cloudtest')) {
-        return 'cloudtest';
-    } else {
-        return '';
-    }
+  if (urlHostName.includes('clouddev')) {
+    return 'a.clouddev';
+  } else if (urlHostName.includes('cloudtest')) {
+    return 'cloudtest';
+  } else {
+    return '';
+  }
 }
