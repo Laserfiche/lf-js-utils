376b4a54b667322dfa6ad870ef2dc5cff79a0b13
alexandria.gomez <alexandria.gomez@WATSONVILLE>
2023-03-20 11:10:12 -0400

    add tests for multiple cookies

--- src/utils/cookie-utils.spec.ts
+++ src/utils/cookie-utils.spec.ts
@@ -1,29 +1,43 @@
-import { getLfLanguageCookie, LfLanguageCookie } from "./cookie-utils";
+import { getLfLanguageCookie, LfLanguageCookie } from './cookie-utils.js';
 
 describe('CookieUtils', () => {
+  it('getLfLanguageCookie should return parsed cookie if in correct format', async () => {
+    const cookiesFromBrowser = 'test=hi; Language=c=es-419|uic=en-US';
 
-    it('getLfLanguageCookie should return parsed cookie if in correct format', async () => {
-        const cookiesFromBrowser = 'test=hi;Language=c=es-419|uic=en-US';
+    const parsedLfCookie: LfLanguageCookie | undefined = getLfLanguageCookie(cookiesFromBrowser);
 
-        const parsedLfCookie: LfLanguageCookie | undefined = getLfLanguageCookie(cookiesFromBrowser);
+    expect(parsedLfCookie).toEqual({ uic: 'en-US', c: 'es-419' });
+  });
 
-        expect(parsedLfCookie).toEqual({uic: 'en-US', c: 'es-419'});
-    });
+  it('getLfLanguageCookie should return undefined if cookie does not exist', async () => {
+    const cookiesFromBrowser = 'test=hi;moretest=cookie';
 
-    it('getLfLanguageCookie should return undefined if cookie does not exist', async () => {
-        const cookiesFromBrowser = 'test=hi;moretest=cookie';
+    const parsedLfCookie: LfLanguageCookie | undefined = getLfLanguageCookie(cookiesFromBrowser);
 
-        const parsedLfCookie: LfLanguageCookie | undefined = getLfLanguageCookie(cookiesFromBrowser);
+    expect(parsedLfCookie).toBeUndefined();
+  });
 
-        expect(parsedLfCookie).toBeUndefined();
-    });
+  it('getLfLanguageCookie should return valid cookie if second language cookie is not valid', async () => {
+    const cookiesFromBrowser = 'test=hi;Language=c=es-419|uic=en-US; no=yes; Language=cookie';
 
-    it('getLfLanguageCookie should return only uic if that is all that exists', async () => {
-        const cookiesFromBrowser = 'test=hi;Language=uic=en-US;moretest=cookie';
+    const parsedLfCookie: LfLanguageCookie | undefined = getLfLanguageCookie(cookiesFromBrowser);
 
-        const parsedLfCookie: LfLanguageCookie | undefined = getLfLanguageCookie(cookiesFromBrowser);
+    expect(parsedLfCookie).toEqual({ uic: 'en-US', c: 'es-419' });
+  });
 
-        expect(parsedLfCookie).toEqual({uic: 'en-US', c: undefined});
-    });
+  it('getLfLanguageCookie should return second valid cookie if second language cookie is valid', async () => {
+    const cookiesFromBrowser = 'test=hi;Language=c=es-419|uic=en-US; no=yes; Language=c=en-US|uic=en-US';
+
+    const parsedLfCookie: LfLanguageCookie | undefined = getLfLanguageCookie(cookiesFromBrowser);
+
+    expect(parsedLfCookie).toEqual({ uic: 'en-US', c: 'en-US' });
+  });
+
+  it('getLfLanguageCookie should return only uic if that is all that exists', async () => {
+    const cookiesFromBrowser = 'test=hi;Language=uic=en-US;moretest=cookie';
+
+    const parsedLfCookie: LfLanguageCookie | undefined = getLfLanguageCookie(cookiesFromBrowser);
+
+    expect(parsedLfCookie).toEqual({ uic: 'en-US', c: undefined });
+  });
 });
-
--- src/utils/cookie-utils.ts
+++ src/utils/cookie-utils.ts
@@ -3,8 +3,13 @@ export interface LfLanguageCookie {
   uic?: string;
 }
 
+const LANGUAGE_COOKIE_PREFIX = 'Language=';
+const C_COOKIE_PREFIX = 'c=';
+const UIC_COOKIE_PREFIX = 'uic=';
+
 /**
- * Function to get UI Culture (uic) and Regional Settings (c) from `Language` cookie, if exists in expected format
+ * Function to get UI Culture (uic) and Regional Settings (c) from `Language` cookie, if exists in expected format.
+ * If multiple valid language cookies, it will return the last instance.
  * @param browserCookies cookies from browser. Cookies separated by `;`
  * @returns LfLanguageCookie, parsed from input
  * @example
@@ -19,12 +24,14 @@ export function getLfLanguageCookie(browserCookies: string): LfLanguageCookie | 
   let cult: LfLanguageCookie | undefined;
   cookiesKeyValue?.forEach((kv) => {
     const trimmedCookieValue = kv?.trim();
-    if (trimmedCookieValue.startsWith('Language=')) {
-      const value: string | undefined = trimmedCookieValue.replace('Language=', '');
-      cult = parseLanguageCookie(value);
+    if (trimmedCookieValue.startsWith(LANGUAGE_COOKIE_PREFIX)) {
+      const value: string | undefined = trimmedCookieValue.replace(LANGUAGE_COOKIE_PREFIX, '');
+      const parsedCult = parseLanguageCookie(value);
+      if (parsedCult) {
+        cult = parsedCult;
+      }
     }
   });
-  // TODO this will return last instance of Language header, is it possible to have multiple?
   return cult ?? undefined;
 }
 
@@ -36,10 +43,10 @@ function parseLanguageCookie(value: string): LfLanguageCookie | undefined {
   let uiCode: string | undefined;
   const cultures = value?.split('|');
   cultures.forEach((code) => {
-    if (code.startsWith('c=')) {
-      langCode = code.replace('c=', '');
-    } else if (code.startsWith('uic=')) {
-      uiCode = code.replace('uic=', '');
+    if (code.startsWith(C_COOKIE_PREFIX)) {
+      langCode = code.replace(C_COOKIE_PREFIX, '');
+    } else if (code.startsWith(UIC_COOKIE_PREFIX)) {
+      uiCode = code.replace(UIC_COOKIE_PREFIX, '');
     }
   });
   if (langCode || uiCode) {
